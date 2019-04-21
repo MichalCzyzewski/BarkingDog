72e461676e342fe6bad80d13e497991e65784bbc
Michal Czyzewski <czy.mich@gmail.com>
2019-04-22 00:15:53 +0200

    second commit

--- src/pl/mczyzewski/BarkingDog.java
+++ src/pl/mczyzewski/BarkingDog.java
@@ -1,4 +1,17 @@
 package pl.mczyzewski;
 
 public class BarkingDog {
-}
+
+    public static boolean hourOfDay(boolean barking, int hourOfDay) {
+
+
+        if (barking == true) {
+            if (hourOfDay >= 8 && hourOfDay <= 22) {
+                return true;
+            } else if (hourOfDay <= 0 && hourOfDay > 23) {
+                return false;
+            }
+        }
+        return false;
+    }
+    }
