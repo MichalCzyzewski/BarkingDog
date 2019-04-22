225706f7189ba52bcfdd148ae27c0304760a7fc3
Michal Czyzewski <czy.mich@gmail.com>
2019-04-22 02:15:04 +0200

    alternative test

--- src/pl/mczyzewski/BarkingDog.java
+++ src/pl/mczyzewski/BarkingDog.java
@@ -3,18 +3,13 @@ package pl.mczyzewski;
 public class BarkingDog {
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
-        if (barking == false) {
-            return false;
-        } else if (hourOfDay < 0) {
-            return false;
-        } else if (hourOfDay <= 7) {
-            return true;
-        } else if (hourOfDay < 23) {
-            return false;
-        } else if (hourOfDay > 23) {
+        if ((hourOfDay < 0 || hourOfDay > 23)) {
             return false;
         }
-        return true;
+        if ((barking) && (hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
+        }
+        return false;
     }
 
 }
